0114e88f4956f4940f95be7b697f9cc005f09df5
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-29 21:44:24 +0000

    Automatically backed up by Learn

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-const MovieShow = props =>
+const MovieShow = ({ movie }) =>
   <div>
     <h3>Movie Show Component!</h3>
   </div>;
